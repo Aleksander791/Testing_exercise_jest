9c4041ce96235631243329803ef5d641def87b3f
Aleksandar Nikola <aleksandar.nikola@laserromae.it>
2025-01-23 12:44:18 +0100

    Statistic exercise

---
+++ unitetest.js
@@ -0,0 +1,17 @@
+test('Should throw error if arrray lenght is empty',()=>{
+    expect()
+})
+
+test('Should throw error if the array contians strings',()=>{
+    expect()
+})
+
+test('Should throw error if is not an array',()=>{
+    expect()
+})
+
+test('Should throw error if variance is a negative number',()=>{
+    expect()
+})
+
+
