5bd1ba9320a61497eb85dca23357fd05454038a8
Aleksandar Nikola <aleksandar.nikola@laserromae.it>
2025-01-23 10:21:49 +0100

    Second commit

--- calculator.js
+++ calculator.js
@@ -1,30 +1,30 @@
 function validateInput(a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-      throw new Error('Input non valido: sono richiesti numeri');
-    }
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Input non valido: sono richiesti numeri');
   }
-  
-  function add(a, b) {
-    validateInput(a, b);
-    return a + b;
+} 
+
+function add(a, b) {
+  validateInput(a, b);
+  return a + b;
+}
+
+function subtract(a, b) {
+  validateInput(a, b);
+  return a - b;
+}
+
+function multiply(a, b) {
+  validateInput(a, b);
+  return a * b;
+}
+
+function divide(a, b) {
+  validateInput(a, b);
+  if (b === 0) {
+    throw new Error('Impossibile dividere per zero');
   }
-  
-  function subtract(a, b) {
-    validateInput(a, b);
-    return a - b;
-  }
-  
-  function multiply(a, b) {
-    validateInput(a, b);
-    return a * b;
-  }
-  
-  function divide(a, b) {
-    validateInput(a, b);
-    if (b === 0) {
-      throw new Error('Impossibile dividere per zero');
-    }
-    return a / b;
+  return a / b;
 }
   
 module.exports = { add, subtract, multiply, divide };
