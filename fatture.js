98f201d0646f5fe15652c9c71cfd6f6b427db839
Aleksandar Nikola <aleksandar.nikola@laserromae.it>
2025-01-28 11:31:34 +0100

    Fatture refactoring

--- fatture.js
+++ fatture.js
@@ -1,54 +1,56 @@
 // 1. Setup iniziale e struttura dati
 class Fattura {
     constructor(id, cliente, importo, data) {
-      if (!id || !cliente || !importo || !data) {
-        throw new Error("Tutti i campi sono obbligatori");
-      }
-      this.id = id;
-      this.cliente = cliente;
-      this.importo = importo;
-      this.data = data;
+        if (!id || !cliente || !importo || !data) {
+            throw new Error("Tutti i campi sono obbligatori");
+        }
+        this.id = id;
+        this.cliente = cliente;
+        this.importo = importo;
+        this.data = data;
     }
-  }
-  
-  // In memoria array per le fatture
-  const fatture = [];
-  
-  // Funzioni CRUD
-  function aggiungiFattura(fattura) {
+}
+
+// In memoria array per le fatture
+const fatture = [];
+
+// Funzioni CRUD
+function aggiungiFattura(fattura) {
     if (!(fattura instanceof Fattura)) {
-      throw new Error("L'oggetto fornito non è una fattura valida");
+        throw new Error("L'oggetto fornito non è una fattura valida");
     }
     fatture.push(fattura);
     return fattura;
-  }
-  
-  function modificaFattura(id, datiModificati) {
+}
+
+function modificaFattura(id, datiModificati) {
     const fattura = fatture.find(f => f.id === id);
     if (!fattura) {
-      throw new Error("Fattura non trovata");
+        throw new Error("Fattura non trovata");
     }
-    Object.assign(fattura, datiModificati);
+    if (datiModificati.cliente !== undefined) fattura.cliente = datiModificati.cliente;
+    if (datiModificati.importo !== undefined) fattura.importo = datiModificati.importo;
+    if (datiModificati.data !== undefined) fattura.data = datiModificati.data;
     return fattura;
-  }
-  
-  function eliminaFattura(id) {
+}
+
+function eliminaFattura(id) {
     const index = fatture.findIndex(f => f.id === id);
     if (index === -1) {
-      throw new Error("Fattura non trovata");
+        throw new Error("Fattura non trovata");
     }
     return fatture.splice(index, 1)[0];
-  }
-  
-  function recuperaFatture() {
+}
+
+function recuperaFatture() {
     return fatture;
-  }
-  
-  // Export per i test
-  module.exports = {
+}
+
+// Export per i test
+module.exports = {
     Fattura,
     aggiungiFattura,
     modificaFattura,
     eliminaFattura,
     recuperaFatture
-  };
+};
